f3e4e9b47efc77bd4ac242ff61e37a24b8f90909
Adam Drake <addrake@ebay.com>
2019-03-14 11:58:53 +0100

    Setting up next.js

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    es6: false
+  },
+  settings: {
+    ecmascript: 6,
+    jsx: true
+  },
+  parserOptions: {
+    ecmaVersion: 2017,
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      experimentalDecorators: true,
+      jsx: true
+    },
+    sourceType: 'module'
+  },
+  plugins: ['react', 'prettier'],
+  extends: ['airbnb', 'prettier'],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'function-paren-newline': 0,
+    'prettier/prettier': ['error'],
+    'import/no-extraneous-dependencies': 0,
+    'import/no-unresolved': 0,
+    'react/no-unescaped-entities': 0,
+    'react/jsx-closing-bracket-location': 0,
+    'react/forbid-prop-types': 0
+  }
+};
